fe489127224dd688ef55db93862cc7c9430241f7
Bob Costas <finkzane@gmail.com>
2018-04-25 12:31:25 -0700

    count_seqs_in_file now takes an open file pointer, file opening/closing is done in read_fasta_lists

--- protein_oligo_library.c
+++ protein_oligo_library.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "protein_oligo_library.h"
 
 char* read_fasta_lists( char* file_to_read )
 {
+    FILE* data_file;
+
+    data_file = fopen( file_to_read, "r" );
+
+    if( !data_file )
+        {
+            printf( "File not found: %s.", file_to_read );
+            exit( EXIT_FAILURE );
+        }
+
+
+    int num_records = count_seqs_in_file( data_file );
+
+    char names[ num_records ];
+    char sequences[ num_records ];
+
+    fclose( data_file );
     return file_to_read;
 }
 
@@ -15,20 +33,16 @@ char* read_fasta_lists( char* file_to_read )
  * @returns int number of sequences in file, or -1 if
  *          file could not be opened
 **/
-int count_seqs_in_file( char* data_file )
+int count_seqs_in_file( FILE* data_file )
 {
-    FILE* open_file;
 
     int counter = 0;
-    char line[ 100 ];
-
-    open_file =  fopen( data_file, "r" );
+    char line[ 256 ];
 
     if( !open_file )
         {
             return -1; 
         }
-
     while( fscanf( open_file, "%s", line ) == 1 )
         {
             if( *( line ) == '>' )
@@ -36,7 +50,5 @@ int count_seqs_in_file( char* data_file )
                     counter++;
                 }
         }
-
-    fclose( open_file );
     return counter;
 }
