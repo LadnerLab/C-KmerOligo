9fe856829ba3e95092e839b59a53331d3aa1b004
Bob Costas <finkzane@gmail.com>
2018-04-25 12:55:08 -0700

    Added missing semicolon

--- protein_oligo_library.h
+++ protein_oligo_library.h
@@ -1,11 +1,13 @@
 #ifndef PROTEIN_OLIGO_H_INCLUDED
 #define PROTEIN_OLIGO_H_INCLUDED
 
-typedef struct SEQUENCE
+typedef struct Sequence
 {
     char* name;
     char* sequence;
 } Sequence;
+
+Sequence* read_fasta_lists( char* file_to_read );
 int count_seqs_in_file( FILE* data_file );
 
 #endif
