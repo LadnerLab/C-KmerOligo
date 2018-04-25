e10b4108920fef9d7e6737d5728e9309d804aaf4
Bob Costas <finkzane@gmail.com>
2018-04-25 13:17:58 -0700

    Changed the way read_fasta lists reads from file, count_seqs_in_file now resets file pointer

--- protein_oligo_library.c
+++ protein_oligo_library.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include "protein_oligo_library.h"
+
+#define LINE_SIZE 256
 
 Sequence* read_fasta_lists( char* file_to_read )
 {
     FILE* data_file;
     int num_records;
+
+    char* str_buffer = malloc( LINE_SIZE * sizeof( char ) );
+    int current_char;
+    int capacity = LINE_SIZE;
+    int num_chars = 0;
+
 
     data_file = fopen( file_to_read, "r" );
 
@@ -16,10 +25,15 @@ Sequence* read_fasta_lists( char* file_to_read )
         }
 
     num_records = count_seqs_in_file( data_file );
+
     Sequence seqs_from_file[ num_records ];
 
-    char names[ num_records ];
-    char sequences[ num_records ];
+    current_char = fgetc( data_file );
+    while( current_char != EOF )
+        {
+            printf( "%c\n", current_char ); 
+            current_char = getc( data_file );
+        }
 
     fclose( data_file );
     return NULL;
@@ -30,6 +44,7 @@ Sequence* read_fasta_lists( char* file_to_read )
 /**
  * Counts the amount of fastas in a fasta file provided
  * 
+ * Note: Resets file pointer
  * @param file_to_read fasta file to be opened and read
  * @returns int number of sequences in file, or -1 if
  *          file could not be opened
@@ -51,5 +66,6 @@ int count_seqs_in_file( FILE* data_file )
                     counter++;
                 }
         }
+    fseek( data_file, 0, SEEK_SET );
     return counter;
 }
