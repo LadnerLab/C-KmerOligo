c6d7adf20386d5abdd7631d46819cd3cfecee5d2
Bob Costas <finkzane@gmail.com>
2018-04-25 12:31:38 -0700

    Added struct sequence

--- protein_oligo_library.h
+++ protein_oligo_library.h
@@ -1,6 +1,11 @@
 #ifndef PROTEIN_OLIGO_H_INCLUDED
 #define PROTEIN_OLIGO_H_INCLUDED
 
-int count_seqs_in_file( char* data_file );
+typedef struct SEQUENCE
+{
+    char* name;
+    char* sequence;
+} Sequence;
+int count_seqs_in_file( FILE* data_file );
 
 #endif
