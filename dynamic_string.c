a14452c19694f197562de717e68526d3978a639c
Bob Costas <finkzane@gmail.com>
2018-04-26 09:39:19 -0700

    Added check_for_resize method

--- dynamic_string.c
+++ dynamic_string.c
@@ -1,6 +1,26 @@
 #include <stdlib.h>
+#include <stdbool.h>
 #include "dynamic_string.h"
 #define DEFAULT_LENGTH 256
+
+/**
+ * Checks whether or not a DynamicString object needs to be resized
+ * if it does, automatically reallocs the object's string buffer
+ * at twice its size
+ * 
+ * @param input DynamicString input to be tested 
+ **/
+void check_for_resize( DynamicString* input )
+{
+    int new_size;
+
+    if( string_length( input->data ) > input->size + 10 )
+        {
+            new_size = input->size * 2;
+            input->data = realloc( input->data, new_size );
+            input->size = new_size;
+        }
+}
 
 int string_length( char[] input )
 {
@@ -13,9 +33,17 @@ int string_length( char[] input )
     return length;
 }
 
-ds_init( DynamicString* input, char string[] )
+void ds_init( DynamicString* input, char string[] )
 {
-    
+    int length = string_length( string );
+
+    if( length >= DEFAULT_LENGTH )
+        {
+
+        }
+    input->data = string;
 }
+
+
 
 
