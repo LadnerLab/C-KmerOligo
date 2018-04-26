d6df4278c8b4bc3ab67564548392cbdfb9251ad1
Bob Costas <finkzane@gmail.com>
2018-04-26 15:31:48 -0700

    Removed unnecessary sizeof call in chec_for_resize

--- dynamic_string.c
+++ dynamic_string.c
@@ -15,10 +15,10 @@ void check_for_resize( DynamicString* input, char string_to_add[] )
     int new_capacity;
     int add_length = string_length( string_to_add );
 
-    if( input->capacity + 10 < input->size + add_length )
+    if( input->capacity + DEFAULT_LENGTH < input->size + add_length )
         {
-            new_capacity = input->capacity * 2;
-            input->data = (char *) realloc( input->data, new_capacity );
+            new_capacity = ( input->capacity ) + DEFAULT_LENGTH;
+            input->data =  realloc( input->data, new_capacity );
             input->capacity = new_capacity;
         }
 }
@@ -37,7 +37,7 @@ int string_length( char* input )
 void ds_init( DynamicString* input )
 {
     input->capacity = DEFAULT_LENGTH;
-    input->data = (char*) malloc( sizeof( char ) * DEFAULT_LENGTH );
+    input->data = malloc( DEFAULT_LENGTH );
     input->size = 0;
 }
 
