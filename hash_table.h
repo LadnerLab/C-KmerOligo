4929ca94259d4d4b754e38c270268285641d0f31
Bob Costas <finkzane@gmail.com>
2018-04-28 16:08:06 -0700

    Fixed bug where HT_Entry was defined in the wrong place, added documentation for Generate_hash

--- hash_table.h
+++ hash_table.h
@@ -1,17 +1,17 @@
 #ifndef HASHTTABLE_HH_INCLUDED
 #define HASHTTABLE_HH_INCLUDED
 
-typedef struct HashTable
-{
-    HT_Entry* table_data; 
-    int capacity;
-} HashTable;
-
 typedef struct HT_Entry
 {
     char* key;
     void* value;
 } HT_Entry;
+
+typedef struct HashTable
+{
+    HT_Entry* table_data; 
+    int capacity;
+} HashTable;
 
 
 /**
@@ -28,5 +28,14 @@ void ht_init( HashTable* table, int size );
  * @param table HashTable object to free
  **/
 void ht_clear( HashTable* table );
+
+/**
+ * Generates the hash for use by the table.
+ * Note: adds the characters in string key up and then bitshifts 
+ * to determine hash value
+ * @param input pointer to HT_Entry object with key to hash
+ * @return integer value representing hash of object
+ **/
+int generate_hash( HT_Entry* input );
 
 #endif
