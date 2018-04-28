b47e8d97a88c196426fe267e8090618f708f4bf9
Bob Costas <finkzane@gmail.com>
2018-04-28 16:07:42 -0700

    Fixed a couple pointer bugs, added generate_hash method

--- hash_table.c
+++ hash_table.c
@@ -1,19 +1,28 @@
+#include <stdlib.h>
 #include "hash_table.h"
 
 void ht_init( HashTable* table, int size )
 {
-    table->data = malloc( sizeof( HT_Entry ) * size ); 
+    table->table_data = malloc( sizeof( HT_Entry ) * size ); 
     table->capacity = size;
 }
 
 void ht_clear( HashTable* table )
 {
-    int index = 0;
-    while( table->table_data[ index ] ) 
-        {
-            free( table->table_data[ index ] );
-            index++;
-        }
+    free( table->table_data->value );
     free( table );
     table = NULL;
+}
+
+
+int generate_hash( HT_Entry* input )
+{
+    int index = 0;
+    int total = 0;
+    while( input->key[ index ] )
+        {
+            total += input->key[ index ] << 3;
+            index++;
+        }
+    return total;
 }
