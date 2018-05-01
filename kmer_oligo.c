f19073ec755a7e249a7ca5f5f2a6417eb9d2cc19
Bob Costas <finkzane@gmail.com>
2018-05-01 11:56:04 -0700

    Added default values for program optionts

--- kmer_oligo.c
+++ kmer_oligo.c
@@ -5,27 +5,33 @@
 
 #include "protein_oligo_library.h"
 
-#define DATA_FILE "test.fast"
-
-
+// program defaults
+#define DEFAULT_XMER_SIZE 100
+#define DEFAULT_YMER_SIZE 100
+#define DEFAULT_REDUNDANCY 1
+#define DEFAULT_STEP_SIZE 1
+#define DEFAULT_PERCENT_VALID 90.00
+#define DEFAULT_ITERATIONS 1
+#define DEFAULT_OUTPUT "output.fasta"
 
 
 int main( int argc, char* argv[] )
 {
 
     // option variables
-    int xmer_window_size = 0;
-    int ymer_window_size = 0;
+    int xmer_window_size = DEFAULT_XMER_SIZE;
+    int ymer_window_size = DEFAULT_YMER_SIZE;
     int min_length = 0;
-    int redundancy = 0;
-    int iterations = 1;
-    float percent_valid = 0;
+    int redundancy = DEFAULT_REDUNDANCY;
+    int iterations = DEFAULT_ITERATIONS;
+    float percent_valid = DEFAULT_PERCENT_VALID;
 
     char* query = NULL;
-    char* output = NULL;
+    char* output = DEFAULT_OUTPUT;
 
     int option;
 
+    // parse options given from command lines
     while( ( option = getopt( argc, argv, "x:y:l:r:i:q:o:" ) ) != -1 )
         {
             switch( option )
@@ -54,9 +60,7 @@ int main( int argc, char* argv[] )
                 }
         }
 
-
-
-    FILE* data_file = fopen( "test.fasta", "r" );
+    FILE* data_file = fopen( query, "r" );
     int index;
 
     if( !data_file )
