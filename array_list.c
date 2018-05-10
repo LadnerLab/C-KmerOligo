72229b24a063e2243bd07313d1162ce82fcc91e0
Bob Costas <finkzane@gmail.com>
2018-05-10 09:53:15 -0700

    Added local check_for_resize_method

--- array_list.c
+++ array_list.c
@@ -1,5 +1,30 @@
+#include <stdlib.h>
+
 #define DEFAULT_CAPACITY 256
 
+
+int check_for_resize( array_list_t* array_check )
+{
+    void* new_data = NULL;
+    size_t new_capacity;
+
+    if( array_check->size + 1 >= array_check->capacity )
+        {
+
+            new_data = realloc( array_check->array_data,  );
+            new_capacity = array_check->capacity * 2;
+
+            if( new_data )
+                {
+                    array_check->array_data = new_data;
+                }
+            else
+                {
+                    return 0;
+                }
+        }
+    return 1;
+}
 void ar_init( array_list_t* to_init )
 {
     if( capacity > 0 )
