d327d3901a4bf6b3bbdfd6c855776d45a03f2d92
Bob Costas <finkzane@gmail.com>
2018-05-14 13:33:46 -0700

    Added set_clear method to header file

--- set.h
+++ set.h
@@ -10,5 +10,6 @@ void set_init( set_t* init );
 void set_add( set_t* set_to_add, char* add_data );
 void set_remove( set_t* set_to_remove, char* remove_data );
 void set_clear( set_t* set_to_clear );
+void set_update( set_t* dest, set_t* source );
 
 #endif
