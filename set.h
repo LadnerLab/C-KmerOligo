5663aec26f10cc0902b835ba20f36d3dbaaf8236
Bob Costas <finkzane@gmail.com>
2018-05-14 16:00:20 -0700

    Added hash_table import and declaration for set_difference

--- set.h
+++ set.h
@@ -1,5 +1,6 @@
 #ifndef SET_H_INCLUDED
 #define SET_H_INCLUDED
+#include "hash_table.h"
 
 typedef struct set_t
 {
@@ -11,5 +12,6 @@ void set_add( set_t* set_to_add, char* add_data );
 void set_remove( set_t* set_to_remove, char* remove_data );
 void set_clear( set_t* set_to_clear );
 void set_update( set_t* dest, set_t* source );
+void set_difference( set_t* dest, set_t* first, set_t* second );
 
 #endif
